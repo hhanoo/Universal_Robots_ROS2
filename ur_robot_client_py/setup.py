a40897e9733d73f543a63d8b22ede7c03dd6d804
hhanoo <woo980711@gmail.com>
2026-09-09 12:55:21 +0900

    [chore] Bump all package versions to 1.4.3

--- ur_robot_client_py/setup.py
+++ ur_robot_client_py/setup.py
@@ -7,7 +7,7 @@ setup(
     # Package metadata
     # =========================================================
     name=package_name,
-    version="1.4.2",
+    version="1.4.3",
     # =========================================================
     # Package discovery
     # Automatically find all Python packages in the directory
